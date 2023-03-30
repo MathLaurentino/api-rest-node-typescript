1a959c1c7a52cd8741762e14ef0daf345079c8d1
Laurentino <matheus.laurentino.ifpr@gmail.com>
2023-03-29 21:15:20 -0300

    add seed to table cidade

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,30 @@
+import { Knex } from "./server/database/knex";
 import { server } from "./server/Server";
 
-server.listen(process.env.PORT || 3333, () => {
-    console.log("App rodando na porta " + process.env.PORT || 3333);
-});
+const startServer = () => {
+    server.listen(process.env.PORT || 3333, () => {
+        console.log("App rodando na porta " + process.env.PORT || 3333);
+    });
+};
+
+
+/**
+ * Se estiver rodando localmente, inicializa as migrates e seed 
+ *  antes de inicializar o servidor com o startServer()
+ */
+if (process.env.IS_LOCALHOST !== "true") {
+    Knex.migrate.latest()
+        .then(() => {
+            Knex.seed.run()
+                .then(() => startServer())
+                .catch(console.log);
+        })
+        .catch(console.log); 
+} else {
+    startServer();
+}
+
+
 
 // yarn start
 // yarn tsc
--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -2,8 +2,12 @@ import supertest from "supertest";
 import { Knex } from "../src/server/database/knex";
 import { server } from "../src/server/Server";
 
+/**
+ * roda a migration e a seed antes de inicializar os testes 
+ */
 beforeAll(async () => {
     await Knex.migrate.latest();
+    await Knex.seed.run();
 });
 
 afterAll(async () => {
