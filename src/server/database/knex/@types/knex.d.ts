788f102f4f270ba0111ce24e4fca6e2e7e4dedb8
Laurentino <matheus.laurentino.ifpr@gmail.com>
2023-03-31 22:56:54 -0300

    add migration to the new table 'usuario'

--- src/server/database/knex/@types/knex.d.ts
+++ src/server/database/knex/@types/knex.d.ts
@@ -1,4 +1,4 @@
-import { ICidade, IPessoa } from "../../models"; // IPessoa, IUsuario
+import { ICidade, IPessoa, IUsuario } from "../../models"; // IPessoa, IUsuario
 
 /**
  * Define a tipagem para o Knex
@@ -7,6 +7,6 @@ declare module "knex/types/tables" {
   interface Tables {
     pessoa: IPessoa;
     cidade: ICidade;
-    // usuario: IUsuario;
+    usuario: IUsuario;
   }
 }
---
+++ src/server/database/migrations/0002_create_usuario.ts
@@ -0,0 +1,34 @@
+/**
+ * Este módulo exporta funções para criar e remover a tabela "usuario" no banco de dados.
+ * @packageDocumentation
+ */
+
+import { Knex } from "knex";
+import { ETableNames } from "../ETableNames";
+
+
+export async function up(knex: Knex) {
+    return knex
+        .schema
+        .createTable(ETableNames.usuario, table => {
+            table.bigIncrements("id").primary().index();
+            table.string("nome").notNullable().checkLength(">", 3);
+            table.string("email").index().notNullable().checkLength(">", 6);
+            table.string("senha").unique().notNullable().checkLength(">", 5);
+
+            table.comment("Tabela usada para armazenar usuarios do sistema.");
+        })
+        .then(() => {
+            console.log(`# Created table ${ETableNames.usuario}`);
+        });
+}
+
+
+export async function down(knex: Knex) {
+    return knex
+        .schema
+        .dropTable(ETableNames.usuario)
+        .then(() => {
+            console.log(`# Dropped table ${ETableNames.usuario}`); 
+        });
+}
